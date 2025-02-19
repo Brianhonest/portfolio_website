ac5adbc88b8e6435aaee87b276020609861e268b
Brianhonest <brianhonestc@gmail.com>
2025-02-19 01:51:50 -0500

    fifth commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from 'react'
-import emailjs from '@emailjs/browser';
-const Contacts = () => {
+import React, { Suspense, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber'
+import Fox from '../models/Fox'
+import Loader from '../components/Loader'
+import { DirectionalLight } from 'three'
+
+const Contact = () => {
   const formRef =  useRef(null);
   const [form,setForm] = useState({name:'',email: '', message:''});
 
@@ -9,20 +14,37 @@ const Contacts = () => {
     setForm({...form,[e.target.name]: e.target.value })
   };
 
-  const handleSumbit = () => {
+  const handleSumbit = (e) => {
     e.preventDefault();
     setIsLoading(true);
     
-    emailjs.sendForm()
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name : "Brian",
+        from_email : form.email,
+        to_email: 'brianhonestc@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+      setForm({name:'',email:'',message:''});
+    }).catch((error) => {
+        setIsLoading(false);
+        console.log(error)
+    })
   }
-  const handleFocus = (e) => {};
+  const handleFocus = () => {};
   const handleBlur = () => {};
  
   return (
     <section className='relative flex lg:flex-row flex-col max-container'> 
       <div className='flex-1 min-w-[50%] flex flex-col'>  
         <h1 className='head-text'>Get in Touch</h1>
-        <form className='w-full flex flex-col gap-7 mt-14' onSubmit={handleSubmit}> 
+        <form className='w-full flex flex-col gap-7 mt-14' onSubmit={handleSumbit}> 
             <label className='text-black-500 font-semibold'>
               Name
               <input type="text" name="name" className='input' placeholder='John' required value={form.name} onChange={handleChange} onFocus={handleFocus} onBlur={handleFocus}/>
@@ -40,8 +62,28 @@ const Contacts = () => {
             </button>
         </form>
       </div>
+
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px] '>
+          <Canvas
+            camera={{
+            position:[0,0,5],
+            fov:75,
+            near:0.1,
+            far:1000
+            }}
+          >
+            <directionalLight intensity={2.5}  position={[0,0,1]}/>
+            <Suspense fallback={<Loader/>}>
+              <Fox
+                position={[0.5,0.35,0]}
+                rotation={[12.6,-0.6,0]}
+                scale={[0.5,0.5,0.5]}
+              />
+            </Suspense>
+          </Canvas>
+      </div>
     </section>
   )
 }
 
-export default Contacts
+export default Contact
